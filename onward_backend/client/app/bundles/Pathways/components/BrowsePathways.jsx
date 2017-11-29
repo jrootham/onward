50a199293ecf0edd7702cfb0827bbc331e025652
Sharon <sharon.peishan.kennedy@gmail.com>
2017-11-29 12:47:16 -0500

    adding react to rails

---
+++ onward_backend/client/app/bundles/Pathways/components/BrowsePathways.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class BrowsePathways extends React.Component {
+  static propTypes = {};
+
+  constructor(props, _railsContext) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        TEST COMPONENT
+      </div>
+    );
+  }
+}
---
+++ onward_backend/client/app/bundles/Pathways/startup/registration.jsx
@@ -0,0 +1,7 @@
+import ReactOnRails from 'react-on-rails';
+
+import BrowsePathways from '../components/BrowsePathways';
+
+ReactOnRails.register({
+  BrowsePathways,
+});
