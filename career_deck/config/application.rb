a6444fa92b249c4be73dd418dc4527c3194e3ae9
Sharon <sharon.peishan.kennedy@gmail.com>
2017-11-30 23:46:44 -0500

    update xframe options

--- career_deck/config/application.rb
+++ career_deck/config/application.rb
@@ -16,5 +16,8 @@ module CareerDeck
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
